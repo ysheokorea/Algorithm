626c1310d82a8ef9d76783a0a90602b2b8717073
ys <ys.heo.korea@gmail.com>
2022-03-07 21:00:31 +0900

    유기농 배추 BFS

--- A_1012.java
+++ A_1012.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.awt.Point;
 
 public class A_1012 {
     public static int[][] matrix;
@@ -9,6 +12,10 @@ public class A_1012 {
     public static int[] new_x = {1, -1, 0, 0};
     public static int[] new_y = {0, 0, -1, 1};
     public static ArrayList<Integer> total = new ArrayList<Integer>();
+    public static Queue<Point> queue;
+    
+
+
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args){
@@ -38,7 +45,7 @@ public class A_1012 {
                     // System.out.print(matrix[0][1]);
                     if(matrix[i][j]==1 && !visit[i][j]){
                         numOfBugs=1;
-                        dfs(j,i);
+                        bfs(j,i);
                         total.add(numOfBugs);
                     }
                 }
@@ -49,6 +56,35 @@ public class A_1012 {
         System.out.println(sb);
         
     }
+    // BFS
+    public static void bfs(int x, int y){
+        queue = new LinkedList<>();
+        queue.add(new Point(x,y));
+        visit[y][x] = true;
+
+
+        while(!queue.isEmpty()){
+            Point temp = queue.poll();
+
+
+            for(int i=0; i<4; i++){
+                int NEW_X = temp.x+new_x[i];
+                int NEW_Y = temp.y+new_y[i];    
+                    // System.out.print(matrix[i][j]);
+                    if(NEW_X>=0 && NEW_Y >=0 && NEW_X<X && NEW_Y<Y){
+                        if(matrix[NEW_Y][NEW_X]==1 && !visit[NEW_Y][NEW_X]){                    
+                            queue.add(new Point(NEW_X,NEW_Y));
+                            visit[NEW_Y][NEW_X]=true;
+                            numOfBugs++;
+                        }
+                    }
+            }
+        }
+            
+        
+    }
+
+    // DFS
     public static void dfs(int x, int y){
         visit[y][x] = true;
 
